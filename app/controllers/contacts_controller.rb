4327b890a2837940932def195b1b3f3bc6dafa63
James <mjameslowell26@gmail.com>
2024-02-17 12:51:14 +0800

    Initial commit: Add Rails application files

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,43 @@
+class ContactsController < ApplicationController
+    def index
+      @contacts = Contact.all
+    end
+  
+    def new
+      @contact = Contact.new
+    end
+  
+    def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+        redirect_to root_path
+      else
+        render 'new'
+      end
+    end
+  
+    def edit
+      @contact = Contact.find(params[:id])
+    end
+  
+    def update
+      @contact = Contact.find(params[:id])
+      if @contact.update(contact_params)
+        redirect_to root_path
+      else
+        render 'edit'
+      end
+    end
+  
+    def destroy
+      @contact = Contact.find(params[:id])
+      @contact.destroy
+      redirect_to root_path
+    end
+  
+    private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :phone)
+    end
+  end
+  
